0215260da21aee9171e022d8607bc24753f5059a
William Faustino da Silva <williamd3v@gmail.com>
2026-09-15 01:13:49 -0300

    fix: use product variant size in cart

--- src/app/store/[storeSlug]/cart/components/cart-summary-item.tsx
+++ src/app/store/[storeSlug]/cart/components/cart-summary-item.tsx
@@ -19,6 +19,7 @@ export default function CartSummaryItem({
   quantity,
   priceInCents,
 }: CartSummaryItemProps) {
+  
   return (
     <>
       <div className="flex items-center justify-between" key={id}>
